1da66f17c3dc90e3445e1979a8de95704fb63684
i513554 <lucy.chi@sap.com>
2020-03-01 16:46:34 -0800

    deploy

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-// server.js
-//
 const express = require('express');
 const PORT = process.env.HTTP_PORT || 8081;
+var AWS = require("aws-sdk");
+
 const app = express();
-app.get('/', (req, res) => {
-    res.send('flowers smell nice');
-});
-app.get('/flower', (req, res) => {
-    res.json({
-      name: 'Dandelion',
-      colour: 'Blue-ish'
-    });
-});
+// app.get('/', (req, res) => {
+//     res.send('flowers smell nice');
+// });
+// app.get('/flower', (req, res) => {
+//     res.json({
+//       name: 'Dandelion',
+//       colour: 'Blue-ish'
+//     });
+// });
+/****** /microservice route START*/
+
+
+/****** /microservice route END*/
+
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}.`);
 });
+
+// AWS.config.update({
+//     region: "eu-west-2",
+//     endpoint: "http://localhost:8000"
+// });
+
+// var docClient = new AWS.DynamoDB.DocumentClient();//
